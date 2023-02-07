2a238bbd41ed101cc2d87463f2dd4873a47e7bfe
vladislav <vl_evlashin@mail.ru>
2023-02-07 20:54:17 +0300

    Add page

--- learning_log/learning_logs/models.py
+++ learning_log/learning_logs/models.py
@@ -10,7 +10,7 @@ class Topic(models.Model):
         return self.text
 
 class Entry(models.Model):
-    #Information
+    # Information
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     text = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
